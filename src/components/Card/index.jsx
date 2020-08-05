45752505538bc0cfe6d475287b86061965eea0d1
Gabriel Oliveira <gabrieloliveira@Gabriels-MBP.lan>
2020-08-05 20:33:30 -0300

    Cards_otimizados

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,12 +4,12 @@ import "./style.card.scss"
 function Card(props) {
     return (
 
-        <div class="card" style={{ width: "18rem;" }}>
+        <div class="card" >
             <img class="card-img-top" src={props.image} alt="Card image cap" />
             <div class="card-body">
                 <h5 class="card-title">{props.title}</h5>
                 <p class="card-text">{props.description}</p>
-                <a href="#" class="btn-primary">Details</a>
+                <a href={props.page} class="btn-primary">Details</a>
             </div>
         </div>
 
